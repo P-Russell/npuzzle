65a3c5ae83b5f80e9272763c656a37df10b1b766
P-Russell <pdrussell011@gmail.com>
2017-11-21 16:16:57 +0200

    start coding heuristi functions

--- data.py
+++ data.py
@@ -5,9 +5,18 @@ from data_from_fd import puzzle_from_fd
 class Data(object):
 
     def __init__(self, size=0, fd=0):
-        super(Data, self).__init__()
         if size:
             self.size = int(size)
             self.puzzle = create.generate_new(self.size)
         elif fd:
             self.size, self.puzzle = puzzle_from_fd(fd)
+
+
+class Node(object):
+
+    def __init__(self, data, ):
+        self.data = data
+
+
+    def gen_new(self):
+        # generates list of new Nodes
